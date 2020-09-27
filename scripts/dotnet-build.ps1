74c5bd5e8264e0c3e11718868d35e93e812cfc92
Jason Gilbertson <jagilber@microsoft.com>
2020-09-26 20:05:36 -0400

    remove publish

--- scripts/dotnet-build.ps1
+++ scripts/dotnet-build.ps1
@@ -1,14 +1,17 @@
 param(
     $project = "$psscriptroot\..\CollectSFData\CollectSFData.csproj",
-    [ValidateSet('Debug','Release')]
-    $configuration = 'Debug',
+    [ValidateSet('Debug', 'Release')]
+    [string[]]$configuration = @('Debug', 'Release'),
     $framework = 'net5',
     $runtime = 'win-x64'
 )
 
 dotnet restore $project
 
-dotnet build $project -c $configuration --framework $framework
+foreach ($config in @($configuration)) {
+    dotnet build $project -c $config --framework $framework
+}
 
-dotnet publish $project -r $runtime -c $configuration --self-contained $true --no-dependencies -p:PublishSingleFile=true -p:PublishedTrimmed=true --framework $framework
+# kusto doesnt like being in singlefile
+#dotnet publish $project -r $runtime -c $configuration --self-contained $true --no-dependencies -p:PublishSingleFile=true -p:PublishedTrimmed=true --framework $framework
 
